a573b2fbbbbcee0c67dd223ddd0e550f1f6176b9
RobertoDelga <robertodelgado842@gmail.com>
2023-09-11 16:36:18 -0600

    Initial commit

---
+++ src/main/java/projectstore/kodigoshoppingstore/KodigoShoppingStoreApplication.java
@@ -0,0 +1,13 @@
+package projectstore.kodigoshoppingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodigoShoppingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodigoShoppingStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectstore/kodigoshoppingstore/KodigoShoppingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package projectstore.kodigoshoppingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodigoShoppingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
